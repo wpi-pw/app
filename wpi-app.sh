ca62f9a2b2810fa20b9dee84026c1c194cc5177d
DimaMinka <cdk@cdk.co.il>
2019-10-17 22:59:02 +0000

    feat--workflow--basic done

--- wpi-app.sh
+++ wpi-app.sh
@@ -22,6 +22,25 @@ if [ "$wpi_init_shell" == "true" ]; then
   done
 fi
 
+# Run workflow install after setup checking
+if [ "$wpi_init_workflow" != "false" ]; then
+
+  # Download default env template or custom from config
+  if [ "$wpi_templates_workflow" == "default" ]; then
+      curl --silent https://raw.githubusercontent.com/wpi-pw/template-workflow/master/wpi-workflow.sh > tmp-template.sh
+  else
+      curl --silent $wpi_templates_workflow > tmp-template.sh
+  fi
+
+  # If template downloaded, run the script
+  if [ -f "${PWD}/tmp-template.sh" ]; then
+      bash ${PWD}/tmp-template.sh $1
+      # delete the script after complete
+      rm ${PWD}/tmp-template.sh
+  fi
+
+fi
+
 # Run shell runner after app install
 if [ "$wpi_init_shell" == "true" ]; then
   for script in "${wpi_shell_after_install[@]}"
