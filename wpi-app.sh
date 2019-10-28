32768f0cb85465d955f85359c0784c5026b98af7
DimaMinka <cdk@cdk.co.il>
2019-10-28 19:01:54 +0000

    chore--wpi-app--prepare to alpha

--- wpi-app.sh
+++ wpi-app.sh
@@ -14,45 +14,54 @@ done
 # Get wpi-source for yml parsing, noroot, errors etc
 source <(curl -s https://raw.githubusercontent.com/wpi-pw/template-workflow/master/wpi-source.sh)
 
+# Create array of scripts
+mapfile -t before_install < <( wpi_yq 'shell.before_install' )
 # Run shell runner before app install
-if [ "$wpi_init_shell" == "true" ]; then
-  for script in "${wpi_shell_before_install[@]}"
+if [ "$(wpi_yq init.shell)" == "true" ]; then
+  for script in "${before_install[@]}"
   do
     bash <(curl -s $(url_path "template-workflow/wpi-shell")) $script
   done
 fi
 
 # Run workflow install after setup checking
-if [ "$wpi_init_workflow" != "false" ]; then
+if [ "$(wpi_yq init.workflow)" != "false" ]; then
   # current environment
   get_cur_env $1
 
   # Download and run default workflow template or custom from the config
-  template_runner $wpi_templates_workflow "template-workflow/wpi-workflow" $wpi_init_workflow $cur_env
+  template_runner $(wpi_yq templates.workflow) "template-workflow/wpi-workflow" $(wpi_yq init.workflow) $cur_env
 
   # Download and run default env template or custom from the config
-  template_runner $wpi_templates_env "template-env/env-init" $wpi_init_env $cur_env
+  template_runner $(wpi_yq templates.env) "template-env/env-init" $(wpi_yq init.env) $cur_env
 
   # Download and run default settings template or custom from the config
-  template_runner $wpi_templates_settings "template-settings/settings-init" $wpi_init_settings $cur_env
+  template_runner $(wpi_yq templates.settings) "template-settings/settings-init" $(wpi_yq init.settings) $cur_env
 
   # Download and run default mu-plugins template or custom from the config
-  template_runner $wpi_templates_mu_plugins "template-mu-plugins/mu-plugins-init" $wpi_init_mu_plugins
+  template_runner $(wpi_yq templates.mu_plugins) "template-mu-plugins/mu-plugins-init" $(wpi_yq init.mu_plugins)
 
   # Download and run default plugins template or custom from the config
-  template_runner $wpi_templates_plugins "template-plugins/plugins-single-init" $wpi_init_plugins
+  template_runner $(wpi_yq templates.plugins_single) "template-plugins/plugins-single-init" $(wpi_yq init.plugins) $cur_env
+
+  # Download and run default plugins bulk template or custom from the config
+  template_runner $(wpi_yq templates.plugins_bulk) "template-plugins/plugins-bulk-init" $(wpi_yq init.plugins_bulk) $cur_env
 
   # Download and run default themes template or custom from the config
-  template_runner $wpi_templates_theme "template-theme/theme-init" $wpi_init_theme
+  template_runner $(wpi_yq templates.theme) "template-theme/theme-init" $(wpi_yq init.theme) $cur_env
 
-#  # Download and run default child theme template or custom from the config
-#  template_runner $wpi_templates_theme "template-child-theme/child-theme-init" $wpi_init_child_theme
-  bash /home/vagrant/apps/wpi.test/template-child-theme/child-theme-init.sh $1
+  # Download and run default child theme template or custom from the config
+  template_runner $(wpi_yq templates.child_theme) "template-child-theme/child-theme-init" $(wpi_yq init.child_theme) $cur_env
+
+  # Download and run default child theme template or custom from the config
+  template_runner $(wpi_yq templates.extra) "template-extra/extra-init" $(wpi_yq init.extra)
 fi
 
+# Create array of scripts
+mapfile -t after_install < <( wpi_yq 'shell.after_install' )
 # Run shell runner after app install
-if [ "$wpi_init_shell" == "true" ]; then
-  for script in "${wpi_shell_after_install[@]}"
+if [ "$(wpi_yq init.shell)" == "true" ]; then
+  for script in "${after_install[@]}"
   do
     bash <(curl -s $(url_path "template-workflow/wpi-shell")) $script
   done
